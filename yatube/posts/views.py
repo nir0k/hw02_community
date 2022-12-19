3188dfa252f8b1ce70114826fb1331d353815c17
Ivan Korbakov <korbakovi@gmail.com>
2022-12-19 20:18:54 +0300

    paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
@@ -19,9 +20,17 @@ def group_posts(request, slug):
 def index(request):
     template = 'posts/index.html'
     title = 'Последние обновления на сайте'
-    posts = Post.objects.order_by('-pub_date')[:POSTS_PER_PAGE]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': title,
+        'page_obj': page_obj,
+    }
+    # posts = Post.objects.order_by('-pub_date')[:POSTS_PER_PAGE]
     return render(
         request,
         template,
-        {'title': title, 'posts': posts, }
+        context
     )
