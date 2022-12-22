c050fabc48a5bd17732b7259e2520a9dcc9f9213
Ivan Korbakov <korbakovi@gmail.com>
2022-12-22 21:15:49 +0300

    after main task in sprint 4

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,13 +1,6 @@
-# from django.shortcuts import render
 # users/views.py
-# Импортируем CreateView, чтобы создать ему наследника
 from django.views.generic import CreateView
-
-# Функция reverse_lazy позволяет получить URL по параметрам функции path()
-# Берём, тоже пригодится
 from django.urls import reverse_lazy
-
-# Импортируем класс формы, чтобы сослаться на неё во view-классе
 from .forms import CreationForm
 
 
